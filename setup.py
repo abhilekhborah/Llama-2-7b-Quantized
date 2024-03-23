115bb6ebd4d1da49bd744f9fa4311d948b5d6130
Abhilekh Borah <abhilekhkey@gmail.com>
2024-03-23 11:51:38 +0530

    llama committed 2

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Llama-7B-deltae',
+    version= '0.0.0',
+    author= 'abhilekh',
+    author_email= 'abhilekhkey@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
